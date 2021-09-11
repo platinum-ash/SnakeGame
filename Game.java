f5651b3c41112f08a90e3f9b9dc63a58c376bcd7
Asharab <87782883+platinum-ash@users.noreply.github.com>
2021-09-12 00:08:01 +0200

    Minor code cleanup

--- Game.java
+++ Game.java
@@ -7,61 +7,74 @@ import java.util.Random;
 public class Game extends JPanel implements ActionListener{
     Timer timer;
     char direction;
-    private static int snakeSize = 3;
+    private static int snakeSize = 3;//Set starting size of the snake
     private static int snakeX[] = new int[50];
     private static int snakeY[] = new int[50];
-    private static int ITEM_SIZE = 20;
-    private static int SCREEN_HEIGHT = 600;
-    private static int SCREEN_WIDTH = 600;
+    private final static int ITEM_SIZE = 20;
+    private final static int SCREEN_HEIGHT = 600;
+    private final static int SCREEN_WIDTH = 600;
     private static boolean gameOver = false;
     Random random = new Random();
-    private int apple[] = {(random.nextInt( (SCREEN_HEIGHT/ITEM_SIZE))* ITEM_SIZE) , (random.nextInt( (SCREEN_HEIGHT/ITEM_SIZE))* ITEM_SIZE)};
+    private int apple[] = new int[2];
     
-
     Game(){   
-        this.setPreferredSize(new Dimension(800, 800));
-        this.setBackground(Color.black);
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
 		this.setFocusable(true);
 		this.addKeyListener(new MyKeyAdapter());
-        init();
+        init();//initialize the game
     }
     public void init(){
-        //set all dot loc
+
+        //Set inital random start values for the food
+        apple[0] = (random.nextInt( (SCREEN_HEIGHT/ITEM_SIZE))* ITEM_SIZE);
+        apple[1] = (random.nextInt( (SCREEN_HEIGHT/ITEM_SIZE))* ITEM_SIZE);
+
+        //Initialize the snake starting position
         for(int i = 0; i < snakeSize; i++){
             //starting from top left
             snakeX[i] = ITEM_SIZE * 4 -  i * ITEM_SIZE; // snake head is at the right the rest behind it to the left
             snakeY[i] = 0; // starting out in a horizontal position
         }
+
+        direction = 'X'; // Make sure there is no movement before first input
         timer = new Timer(200, this);
         timer.start();
     }
+
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        //Do all the drawing here
         drawBoard(g);
     }
+    
+    //Do all the drawing here
     public void drawBoard(Graphics g){
-
         setBackground(Color.gray);
-       
-        //Draw all the snake Dots at their respective positions
-        g.setColor(Color.green);
+        //Draw all the snake squares at their respective positions
         for(int i = 0; i < snakeSize; i++){
+            g.setColor(Color.green);
+            //Use a different color for snake head
+            if(i == 0){
+                g.setColor(Color.RED);
+            }
             g.fillRect(snakeX[i], snakeY[i],ITEM_SIZE, ITEM_SIZE);
         }
         //Draw the food
-        g.setColor(Color.GREEN);
+        g.setColor(Color.BLUE);
         g.fillRect(apple[0], apple[1], ITEM_SIZE, ITEM_SIZE);
     }
 
     public void move(){
         checkFood();
         //Move the snake according to the input
-        //Each snake item moves to the position of the item before it
-        for(int i = snakeSize - 1; i > 0; i--){
-            snakeX[i] = snakeX[i - 1];
-            snakeY[i] = snakeY[i - 1];
+        //Temporary fix to prevent snake from moving at the start
+        if(direction != 'X'){
+            //Each snake item moves to the position of the item before it
+            for(int i = snakeSize - 1; i > 0; i--){
+                snakeX[i] = snakeX[i - 1];
+                snakeY[i] = snakeY[i - 1];
+            }
         }
+        
         switch(direction){
             case 'R':
                 snakeX[0] += ITEM_SIZE;
@@ -77,31 +90,33 @@ public class Game extends JPanel implements ActionListener{
                 break;
         }
     }
-    public void checkFood(){
 
+    public void checkFood(){
         //check after each move if snake head is at food
-        if(apple[0] == snakeX[0] && apple[1] == snakeY[1]){
-            //increase snake size
-            snakeSize++;
+        if(apple[0] == snakeX[0] && apple[1] == snakeY[1]){   
+            snakeSize++; //increase snake size
+
             //spawn another apple
             apple[0] = (random.nextInt( (SCREEN_HEIGHT/ITEM_SIZE))* ITEM_SIZE);
             apple[1] = (random.nextInt( (SCREEN_HEIGHT/ITEM_SIZE))* ITEM_SIZE);
-            System.out.println("The x coordinate is : " + apple[0] + " the y coordinate is : "+ apple[1]);
         }       
     }
+
     public void validateMov(){
         //game logic implemented here
+
         //Check bounds first
         if(snakeX[0] == 0 || snakeX[0] == SCREEN_WIDTH - ITEM_SIZE){
             gameOver = true;//game is over if snake head touches the screen borders
         }
         //Perform same checks along the Y-axis
         if(snakeY[0] < 0 || snakeY[0] == SCREEN_HEIGHT - ITEM_SIZE){
-            gameOver = true;
+            gameOver = true;//game is over if snake head touches the screen borders
         }
-        //For debugging
+        
         if(gameOver){
-            System.out.println("Game over");
+            //Stop the game if game ends according to the rules
+            timer.stop();
         }
     }
   
@@ -131,9 +146,7 @@ public class Game extends JPanel implements ActionListener{
                 case KeyEvent.VK_DOWN:
                     direction = 'D';
                     break;
-                }
-        
-        
+            }
         }
     }
 }
